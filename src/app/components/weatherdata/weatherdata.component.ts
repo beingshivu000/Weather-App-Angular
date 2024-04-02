01ec6ef870ed2a7e9b2db449fdac4862b74e4607
shivani <shivanirm8119@gmail.com>
2024-04-02 16:46:11 +0530

    added

--- src/app/components/weatherdata/weatherdata.component.ts
+++ src/app/components/weatherdata/weatherdata.component.ts
@@ -46,7 +46,6 @@ export class Weatherdata implements OnInit {
   onSubmit(form: FormGroup) {
     this.sendCityName(form.value.cityName);
     this.modelCityName = form.value.cityName;
-    this.myForm.reset();
   }
 
   sendCityName(cityName: string) {
